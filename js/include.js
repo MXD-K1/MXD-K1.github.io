c2e2dc460b96d7065773ae7de50b3ac3b5b83596
Mohammed Al-Shugaa <hmdoonwork71@gmail.com>
2026-06-02 13:44:56 +0300

    huge progress part 2

--- js/include.js
+++ js/include.js
@@ -1,21 +1,11 @@
-const meta = document.createElement("meta");
-meta.charset = "utf-8";
-document.head.appendChild(meta);
-
-const link = document.createElement("link");
-link.rel = "stylesheet";
-link.href = "./style.css";
-document.head.appendChild(link);
-
-const iconLink = document.createElement("link");
-iconLink.rel = "stylesheet";
-iconLink.href = "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
-document.head.appendChild(iconLink);
-
+const header = document.createElement("header");
 fetch("./shared/header.html")
     .then(res => res.text())
-    .then(data => document.querySelector("header").innerHTML = data);
+    .then(data => header.innerHTML = data);
+document.body.prepend(header);
 
+const footer = document.createElement("footer");
 fetch("./shared/footer.html")
     .then(res => res.text())
-    .then(data => document.querySelector("footer").innerHTML = data);
+    .then(data => footer.innerHTML = data);
+document.body.appendChild(footer);
